f9347d7c012bc98c7ca19fa0cff2056a86550780
Theo-FERRETE <theo.ferrete@laplateforme.io>
2025-04-28 13:41:47 +0200

    mise en français du formulaire

--- register.php
+++ register.php
@@ -63,10 +63,10 @@ if (
     <input type="text" name="Adresse" placeholder="Adresse" required><br>
     <input type="email" name="email" placeholder="Email" required><br>
     <input type="password" name="Mot de passe" placeholder="Mot de Passe" required><br>
-    <button type="submit">Register</button>
+    <button type="submit">Inscription</button>
 </form>
 
-<p>Already have an account? <a href="login.php">Login here</a></p>
+<p>Si déja un compte ? <a href="login.php">Connecte-toi</a></p>
 
 </body>
 </html>
